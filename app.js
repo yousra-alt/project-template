9ebe3761d97cee36298823401ae9708db5129919
yousra-alt <123966037+yousra-alt@users.noreply.github.com>
2023-03-20 13:07:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("app.js is running 🎉");
+"use strict"
+
+// 5 > 4; TRUE
+// "apple" > "pineapple"; FALSE
+// "2" > "12"; TRUE
+// undefined == null; TRUE
+// undefined === null; FALSE
+// null == "\n0\n"; FALSE
+// null === +"\n0\n"; FALSE
+// "race" === "petl"; FALSE
+// "race" !== "petl"; TRUE
+
+const result = "race" !== "petl";
+console.log (result)
+
+
